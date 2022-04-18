633eeb61b18dd3c4c119d5bf247e1258d96f61f2
rtjshreyd <shreyanshece1041@gmail.com>
2022-04-18 05:55:00 +0530

    education set

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -7,36 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
   timeline = [
-    // {
-    //   heading: "Infosys",
-    //   duration: "Present",
-    //   subtitle: "",
-    //   content: "Upcoming Full stack develover at Infosys!",
-    //   className1: "mar-left",
-    //   className2: "prt_about_learnbox_right"
-    // },
     {
-      heading: "B.Tech - Computer Science & Engineering",
-      duration: "2020",
-      subtitle: "GLA University, Mathura",
-      content: "Successfully completed my B.tech in Computer science from GLA University, Mathura.",
+      heading: "B.Tech - Electronics and Communication Engineering",
+      duration: "2017",
+      subtitle: "Dr APJ Abdul Kalam Technical University, Lucknow, UP, India",
+      content: "Successfully completed my B.tech in ECE from AKTU, Lucknow.",
       className1: "mar-right",
       className2: "prt_about_learnbox_left"
     },
     {
       heading: "Higher Secondary(12th)",
-      duration: "2016",
+      duration: "2013",
       subtitle: "CBSE Board",
-      content: "Successfully completed my intermediate studies in 2016 from Gyan Deep Sr. Secondary Public School, Shikohabad.\n" +
+      content: "Successfully completed my intermediate studies in 2013 from Tagore Public School, Prayagraj, UP, India.\n" +
         "Major: PCM.",
       className1: "mar-left",
       className2: "prt_about_learnbox_right"
     },
     {
       heading: "Secondary Stage(10th)",
-      duration: "2014",
+      duration: "2011",
       subtitle: "CBSE Board",
-      content: "Successfully completed my high school studies in 2014 from Georgions Academy, Shikohabad.",
+      content: "Successfully completed my high school studies in 2011 from Tagore Public School, Prayagraj, UP, India.",
       className1: "mar-right",
       className2: "prt_about_learnbox_left"
     }
